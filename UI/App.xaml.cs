42729a873367083fe6f87af94a5e8c273f8146aa
Keenan Marco <marcoke58@uwosh.edu>
2023-10-29 11:38:14 -0500

    #Changed the namespaces for AppShell and App to make the emulator work

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -6,7 +6,8 @@
         {
             InitializeComponent();
 
-            MainPage = new WelcomePage();
+            MainPage = new NavigationPage(new WelcomePage());
+            ;
         }
     }
 }
