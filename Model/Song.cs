931369c9b499bc05b7403b42dc5d887ca2ff0986
Keenan Marco <marcoke58@uwosh.edu>
2023-10-30 12:24:19 -0500

    #Added new song object

---
+++ Model/Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototypes.Model
+{
+    public class Song
+    {
+        private String _title;
+        private String _artist;
+        private int _duration;
+        public Song(String title, String artist, int duration)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+        }
+
+        public String Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public String Artist
+        {
+            get { return _artist; }
+            set { _artist = value; }
+        }
+
+        public int Duration
+        {
+            get { return _duration; }
+            set { _duration = value; }
+        }
+    }
+}
