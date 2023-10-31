9c6de98d0b54bbd88519a8f71d072e3867bf8d8f
Keenan Marco <marcoke58@uwosh.edu>
2023-10-31 14:38:24 -0500

    #Edit songs work

--- UI/EditSongPopup.xaml.cs
+++ UI/EditSongPopup.xaml.cs
@@ -7,15 +7,18 @@ public partial class EditSongPopup : ContentPage
     private Song _song;
 	public EditSongPopup(Song song)
 	{
-		Song _song = song as Song;
+        
         InitializeComponent();
-	}
+        _song = song;
+    }
     private void EditSong(object sender, EventArgs e)
     {
         string songName = songNameEntry.Text;
         string artistName = artistNameEntry.Text;
         int duration = Int32.Parse(durationEntry.Text);
-        int setlistId = _song.SetlistId;
+        int setlistId = 0;
+            setlistId = _song.SetlistId;
+        
         string oldSongName = _song.Title;
         string oldArtist = _song.Artist;
         MauiProgram.StageManagerBL.EditSong(setlistId, oldSongName, oldArtist, songName, artistName, duration);
