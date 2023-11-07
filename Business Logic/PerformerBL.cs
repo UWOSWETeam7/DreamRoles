272faf1bd69dec7ce3d9290d0e38d941fe07516f
Keerthana Ambati <ambatk18@uwosh.edu>
2023-11-07 14:23:01 -0600

    Started work on performerBL and performerDB

--- Business Logic/PerformerBL.cs
+++ Business Logic/PerformerBL.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Prototypes.Model;
+using Prototypes.Business_Logic.IBusinessLogic;
+using Prototypes.Database;
+using Prototypes.Model.Interfaces;
+using System.Collections.ObjectModel;
 
 namespace Prototypes.Business_Logic
 {
-    internal class PerformerBL
+    class PerformerBL : IPerformerBL
     {
+        private IPerformerDB PerformerDB = new PerformerDB();
+
+        int performerAccessCode = 001122;
+        public Boolean IsPerformer(int accessCode)
+        {
+            Boolean isPerformer = false;
+            if (accessCode == performerAccessCode)
+            {
+                isPerformer = true;
+            }
+            return isPerformer;
+        }
+
+        public PerformerBL()
+        {
+            PerformerDB = new PerformerDB();
+        }
+
+        public void UpdateStatus(Performer performer)
+        {
+
+        }
     }
 }
--- Database/PerformerDB.cs
+++ Database/PerformerDB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace Prototypes.Database
 {
-    internal class PerformerDB
-    {
+    class PerformerDB : IPerformerDB
+    { 
+        //The string to connect to the database
+        private String _connString;
+
+        static String GetConnectionString()
+        {
+            var connStringBuilder = new NpgsqlConnectionStringBuilder();
+            connStringBuilder.Host = "airport-table-13041.5xj.cockroachlabs.cloud";
+            connStringBuilder.Port = 26257;
+            connStringBuilder.SslMode = SslMode.VerifyFull;
+            connStringBuilder.Username = "keenan_marco";
+            connStringBuilder.Password = "6tRK2gvZOx62cwwPBe8znA";
+            connStringBuilder.Database = "dreamroles";
+            connStringBuilder.ApplicationName = "whatever";
+            connStringBuilder.IncludeErrorDetail = true;
+
+
+            return connStringBuilder.ConnectionString;
+        }
     }
 }
