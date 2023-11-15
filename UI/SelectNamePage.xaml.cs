6efe766863bb0867d77281c3d77a7c82bd78cf2c
Keerthana Ambati <ambatk18@uwosh.edu>
2023-11-15 11:10:48 -0600

    changed select name page

--- UI/SelectNamePage.xaml.cs
+++ UI/SelectNamePage.xaml.cs
@@ -5,19 +5,23 @@ namespace Prototypes.UI;
 //@author: Keerthana Ambati
 public partial class SelectNamePage : ContentPage
 {
+    Performer performer;
     public SelectNamePage()
     {
         InitializeComponent();
         BindingContext = new SearchBarPerformerViewModel();
 
         //show performer names in the dropdown
-        //once user selects their name, navigate to PerformerHomePage.xaml
+        //once user selects their name, navigate to PerformerHomePage.xaml for that specific performer
     }
 
     private void OnNameSelected(object sender, EventArgs e)
     {
         var label = (Label)sender;
-        var performer = (Performer)label.BindingContext;
+        performer = (Performer)label.BindingContext;
+    }
+    private void OnNextClicked(object sender, EventArgs e)
+    {
         Navigation.PushAsync(new PerformerHomePage(performer));
     }
 
