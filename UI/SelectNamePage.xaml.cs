6067cf53ef2251dc1ac35029becf84589c730be6
Keenan Marco <marcoke58@uwosh.edu>
2023-11-15 15:42:57 -0600

    #Edited some UI

--- UI/SelectNamePage.xaml.cs
+++ UI/SelectNamePage.xaml.cs
@@ -24,7 +24,7 @@ public partial class SelectNamePage : ContentPage
     }
     private void OnNextClicked(object sender, EventArgs e)
     {
-        db.CheckInPerformer(performer, "checked in");
+        //db.CheckInPerformer(performer, "checked in");
         Navigation.PushAsync(new PerformerHomePage(performer));
         //change checked in status of this performer
     }
