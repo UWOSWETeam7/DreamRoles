1e2fe642ac9b0b8cf851de3a8fd53bf07142b3b8
Keenan Marco <marcoke58@uwosh.edu>
2023-11-28 17:39:08 -0600

    Can add performers

--- Databases/Interface/IDatabase.cs
+++ Databases/Interface/IDatabase.cs
@@ -37,7 +37,7 @@ namespace Prototypes.Databases.Interface
         /// </summary>
         /// <param name="performer">the performer that the user wants to put into the file</param>
         /// <returns>True if it was inserted into the ObservableCollection and the file, false otherwise</returns>
-        public Boolean InsertPerformer(String firstName, String lastName, ObservableCollection<ISongDB> songs, String email, String phoneNumber, int absences);
+        public Boolean InsertPerformer(String firstName, String lastName, ObservableCollection<ISongDB> songs, String email, int phoneNumber, int absences);
 
         /// <summary>
         /// Deletes the performer in the file with the given id
