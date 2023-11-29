b78baf8f3fc9bd956007a9a2608fbb32edf9e848
Keith Thoong <thoonk78@uwosh.edu>
2023-11-29 14:11:39 -0600

    - Changed MAnagerHomePage to navigate to MAnagerPerformerInfoPage by clicking on the section of the performer instead of the label

--- UI/ManagerHomePage.xaml.cs
+++ UI/ManagerHomePage.xaml.cs
@@ -33,11 +33,15 @@ public partial class ManagerHomePage : ContentPage
         }
     }
 
-    private void ShowManagerPerformerInfoPage(object sender, EventArgs e)
+    private async void OnPerformerSelectionChange(object sender, SelectionChangedEventArgs e)
     {
-        var boxView = (BoxView)sender;
-        var performer = (Performer) boxView.BindingContext;
-        Navigation.PushAsync(new ManagerPerformerInfoPage(performer));
+        if (e.CurrentSelection.Count > 0)
+        {
+            Performer performer = (Performer)e.CurrentSelection[0];
+            await Navigation.PushAsync(new ManagerPerformerInfoPage(performer));
+            CVPerformers.SelectedItem = null;
+
+        }
     }
 
     private void ShowSongListPage(object sender, EventArgs e)
