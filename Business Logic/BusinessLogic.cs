ad1b49388a9449f1a915e3ac2e54d5f27961f1d7
Keerthana Ambati <ambatk18@uwosh.edu>
2023-11-29 18:11:44 -0600

    comments for BL and IBL

--- Business Logic/BusinessLogic.cs
+++ Business Logic/BusinessLogic.cs
@@ -15,6 +15,55 @@ namespace Prototypes.Business_Logic
         private IDatabase Database = new Database();
 
         /// <summary>
+        /// Initializes a Database when the program starts 
+        /// </summary>
+        public BusinessLogic()
+        {
+            Database = new Database();
+        }
+
+        /// <summary>
+        /// Gets the ObservableCollection of all performers
+        /// </summary>
+        public ObservableCollection<Performer> Performers
+        {
+            get { return Database.SelectAllPerformers(2023); }
+        }
+
+        /// <summary>
+        /// Gets the ObservableCollection of all songs
+        /// </summary>
+        public ObservableCollection<Song> Songs
+        {
+            get { return Database.SelectAllSongs(); }
+        }
+
+        /// <summary>
+        /// Gets the ObservableCollection performers
+        /// </summary>
+        public ObservableCollection<Performer> GetCheckedInPerformers
+        {
+            get { return Database.GetCheckedInPerformers(); }
+        }
+
+        /// <summary>
+        /// Gets the ObservableCollection of all not checked in performers
+        /// </summary>
+        public ObservableCollection<Performer> GetNotCheckedInPerformers
+        {
+            get { return Database.GetNotCheckedInPerformers(); }
+        }
+
+        /// <summary>
+        /// Gets the ObservableCollection of all rehearsals
+        /// </summary>
+        public ObservableCollection<Rehearsal> Rehearsals
+        {
+            get { return Database.GetAllRehearsals(); }
+        }
+
+
+        /// <summary>
         /// This method adds an entry into the rehearsal table, namely the time of the rehearsal and the song 
         /// they are rehearsing
         /// </summary>
@@ -96,6 +145,13 @@ namespace Prototypes.Business_Logic
             }
             return "Invalid length of input.";
         }
+
+        /// <summary>
+        /// This method updates an exisiting song's name given its old name to identify it and the new one
+        /// </summary>
+        /// <param name="oldSongName">The name of the song to update (its old name)</param>
+        /// <param name="newSongName">The new name of the song</param>
+        /// <returns>String- null if update was successful or error message if not</returns>
         public String EditSong(String oldSongName, String newSongName)
         {
             if (newSongName.Length <= 255)
@@ -113,6 +169,11 @@ namespace Prototypes.Business_Logic
             return "Invalid length of input";
         }
 
+        /// <summary>
+        /// This method adds a given song
+        /// </summary>
+        /// <param name="title">The title of the song to add</param>
+        /// <returns>String- null if addition was successful or error message if not</returns>
         public String AddSong(String title)
         {
             if(title.Length <= 255)
@@ -129,39 +190,23 @@ namespace Prototypes.Business_Logic
             }
             return "Invalid length of input";
         }
+        
         /// <summary>
-        /// Gets the ObservableCollection performers
+        /// This method deletes a song given its title
         /// </summary>
-        public ObservableCollection<Performer> Performers
-        {
-            get { return Database.SelectAllPerformers(2023); }
-        }
-        public ObservableCollection<Performer> CheckedInPerformers
-        {
-            get { return Database.GetCheckedInPerformers(); }
-        }
-        public ObservableCollection<Song> Songs
-        {
-            get { return Database.SelectAllSongs(); }
-        }
-        public ObservableCollection<Performer> GetCheckedInPerformers
-        {
-            get { return Database.GetCheckedInPerformers(); }
-        }
-
-        public ObservableCollection<Performer> GetNotCheckedInPerformers
-        {
-            get { return Database.GetNotCheckedInPerformers(); }
-        }
-        public ObservableCollection<Rehearsal> Rehearsals
-        {
-            get { return Database.GetAllRehearsals(); }
-        }
+        /// <param name="songTitle">The title of the song to delete</param>
+        /// <returns>boolean true or false if delete was successful or not</returns>
         public Boolean DeleteSong(String songTitle)
         {
             return Database.DeleteSong(songTitle);
         }
 
+        /// <summary>
+        /// This method adds a song for the specified performer
+        /// </summary>
+        /// <param name="userId">The id of the performer</param>
+        /// <param name="songName">The song name</param>
+        /// <returns>String- null if addition was successful or error message if not</returns>
         public String AddSongForPerformer(int userId, String songName)
         {
             bool answer = Database.InsertSongForPerformer(userId, songName);
@@ -176,6 +221,13 @@ namespace Prototypes.Business_Logic
             
         }
 
+        /// <summary>
+        /// This method edits a performer's contact information, namely their phone number and email
+        /// </summary>
+        /// <param name="userId">The id of the performer whose information you are editing</param>
+        /// <param name="phoneNumber">The phone number for the performer</param>
+        /// <param name="email">The email for the performer</param>
+        /// <returns>String- null if edit was successful or error message if not</returns>
         public String EditPerformerContact(int userId, String phoneNumber, String email)
         {
             if (email.Length <= 255 || phoneNumber.Length <= 14)
@@ -195,14 +247,6 @@ namespace Prototypes.Business_Logic
             return "Invalid length of input.";
             //Sends it to the Database
             
-        }
-
-        /// <summary>
-        /// Initializes a Database when the program starts 
-        /// </summary>
-        public BusinessLogic()
-        {
-            Database = new Database();
         }
 
         /// <summary>
@@ -292,11 +336,20 @@ namespace Prototypes.Business_Logic
             return Database.CheckInPerformer(performer, status);
         }
 
+        /// <summary>
+        /// This method updates a performer's status- whether they are checked in, not checked in, or excused
+        /// </summary>
+        /// <param name="performer">The name of the performer</param>
+        /// <param name="status">The status- checked in, not checked in, or excused</param>
+        /// <returns>a tuple of a boolean based on the success of updating and a message</returns>
         public (bool success, string message) UpdatePerformerStatus(Performer performer, String status)
         {
             return Database.UpdatePerformerStatus(performer, status);
         }
 
+        /// <summary>
+        /// This method generates a new 7 digit access code for the performers and calls the database method to change the code
+        /// </summary>
         public void GenerateNewAccessCode()
         {
             Random rand = new Random();
@@ -304,16 +357,32 @@ namespace Prototypes.Business_Logic
             Database.UpdatePerformerAccessCode(newCode);
         }
 
+        /// <summary>
+        /// This method gets all rehearsals
+        /// </summary>
+        /// <returns>an observable collection of all rehearsals</returns>
         public ObservableCollection<Rehearsal> GetAllRehearsals()
         {
             return Database.GetAllRehearsals();
         }
 
+        /// <summary>
+        /// This method gets all rehearsals that the particular performer is involved in
+        /// </summary>
+        /// <param name="performer">The performer whose rehearsals you want to get</param>
+        /// <returns>The observable collection of all of that performer's rehearsals</returns>
         public ObservableCollection<Rehearsal> GetPerformerRehearsals(Performer performer)
         {
             return Database.GetPerformerRehearsals(performer);
         }
 
+        /// <summary>
+        /// This method updates whether or not a performer is checked in for a specific rehearsal or not
+        /// </summary>
+        /// <param name="performer">The performer</param>
+        /// <param name="rehearsal">The rehearsal</param>
+        /// <param name="isCheckedIn">true or false if they are checked in or not for that rehearsal</param>
+        /// <returns>a tuple of boolean true or false if the status was updated and a message</returns>
         public (bool success, string message) UpdatePerformerRehearsalStatus(Performer performer, Rehearsal rehearsal, bool isCheckedIn)
         {
             return Database.UpdatePerformerRehearsalStatus(performer, rehearsal, isCheckedIn);
