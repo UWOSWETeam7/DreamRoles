dbe5596017444b8d3a8675d1d1e33393e0c889db
Keenan Marco <marcoke58@uwosh.edu>
2023-12-14 13:24:13 -0600

    Fixed sign out error

--- UI/ManagerHomePage.xaml.cs
+++ UI/ManagerHomePage.xaml.cs
@@ -86,7 +86,17 @@ public partial class ManagerHomePage : ContentPage
     private async void SignOutButton_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new WelcomePage());
-        Navigation.RemovePage(this);
+
+        // Remove all pages from the navigation stack except the WelcomePage
+        var existingPages = Navigation.NavigationStack.ToList();
+
+        if (existingPages.Any())
+        {
+            foreach (var page in existingPages.Take(existingPages.Count - 1))
+            {
+                Navigation.RemovePage(page);
+            }
+        }
     }
 
 }
